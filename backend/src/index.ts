d791f71cf471334bea0b1dd2db61b86946a14c00
annette Bwemere <bwemereannette@gmail.com>
2022-11-05 12:05:49 +0200

    feat: autentificate + log in +  register new users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,8 +7,12 @@ import cors from 'cors' ;
 import passport from 'passport';
 import User  from './User';
 import  dotenv from 'dotenv';
+import {UserInterface} from "./interfaces/Userinterfaces";
+// import {LocalStrategy} from 'passport-local';
+// import LocalStrategy from "passport-local";
 
-dotenv.config();
+ const LocalStrategy = passport-local().Strategy;
+ dotenv.config();
 
 
 const  URI = process.env.MONGODB_URI;
@@ -28,6 +32,8 @@ console.log(URI)
     }) ;
 
    
+
+  // midlewere
 const app: Express = express();
 app.use(express.json())
     .use(cors({origin: "http://localhost:3000", credentials: true}))
@@ -42,22 +48,81 @@ app.use(express.json())
 app.use(cookieParser())
     .use(passport.initialize())
     .use(passport.session());
+//passport
+passport.use(new  LocalStrategy((username: any, paassword: string, done: (arg0: null, arg1: boolean) => void)=>{
+  User.findOne({ username: username}, (err: any, user : any)=>{
+    if (err) throw err;
+    if (!user) return done(null,false);
+    bscrypt.compare(paassword, user.paassword, (err, result)=>{
+      if(err) throw err;
+      if(result === true){
+        return done (null, user);
+      }else {
+        return done(null, false);
+      }
+    });
+  });
+})
+);
+
+
+passport.serializeUser((user : any, cb)=>{
+  cb(null,user.id);
+});
+
+
+passport.deserializeUser((id: string, cb)=>{
+  User.findOne({_id: id}, (err: any, user : any) =>{
+    const userInformation = {
+      username : user.username,
+      isAdmin: user.isAdmin
+    };
+    cb(err, userInformation);
+});
+})
+
+
+
 
 const port = process.env.PORT
 
-console.log(port);
-
 app.post('/register', async (req: Request, res: Response) => {
-    // res.send('Salut tous les mondes')
-    const hashedPassword = await bscrypt.hash(req.body.password, 10);
-    const newUser = new User({
-        username: req.body.username,
-        password: hashedPassword
-    });
- await newUser.save();
-    res.send("Fait avec succes connection utilisateur Annette")
+
+   const {username, password} = req?.body;
+   if (username || !password || typeof username !== "string" || typeof password !== "string") {
+      res.send("N'oublies pas les conténus");
+      return;
+   }
+   User.findOne({ username}, async (err: Error, doc? : UserInterface) =>{
+    if (err)throw err;
+    if (doc) res.send("User exists déjà veuillez changer")
+    if (!doc) {
+
+      const hashedPassword = await bscrypt.hash(password, 10);
+      const newUser = new User({
+          username,
+          password: hashedPassword
+      });
+   await newUser.save();
+      res.send("Fait avec succes connection utilisateur Annette")
+       }
+  })
 });
 
+
+
+app.post("/login", passport.authenticate("local", (req, res) => {
+  res.send("Autentifier avec succes")
+}));
+
+
+app.get("/user", (req,res)=>{
+  res.send(req.user);
+})
 app.listen(4000, () => {
     console.log(`[server]: Server is runnning at https : anny  localhost: ${port}`);
 })
+
+function local() {
+  throw new Error('Function not implemented.');
+}
