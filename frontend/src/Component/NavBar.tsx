62e27ed958b0f2d85cf1ac04873e4109e807f380
annette Bwemere <bwemereannette@gmail.com>
2022-11-09 09:16:40 +0200

    feat: condition au routes

--- frontend/src/Component/NavBar.tsx
+++ frontend/src/Component/NavBar.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Link} from 'react-router-dom'
 import {myContext} from "../Pages/Context"
 export default function NavBar() {
+  const ctx = useContext(myContext);
   return (
-    
     <div className='NavContainer'>
       {
-        Ctx ? (
+        ctx ? (
           <>
           <Link to="/logout">Logout</Link>
           <Link to="/admin">Admin</Link>
@@ -14,10 +14,9 @@ export default function NavBar() {
           </>
         ) : ( 
           <>
-          <Link to="/home">Home</Link>
-      
-        
+          <Link to="/">Home</Link>
           <Link to="/login">login</Link>
+          <Link to="/home">Home</Link>
           <Link to="/register">Register</Link>
           </>
         )
