51b55a9da50b23e48cb08b2480987503de3d9569
annette Bwemere <bwemereannette@gmail.com>
2022-11-29 15:11:18 +0200

    style compon

--- frontend/src/Pages/login/Login.tsx
+++ frontend/src/Pages/login/Login.tsx
@@ -65,7 +65,7 @@ export default function Login() {
         <div>
           <a href="/">
             <h3 className="text-4xl font-bold text-purple-600">
-            <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=timenewreman&weight=900&duration=2500&pause=1000&color=1E1DF7&background=8672FF00&width=435&lines=Welcom+To+AnnyChatt" alt="Typing SVG" /></a>t
+            <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=timenewreman&weight=700&size=33&duration=2500&pause=1000&color=7C3AED&background=8672FF00&width=446&lines=Welcom+To+AnnyChatt" alt="Typing SVG" /></a>
             </h3>
           </a>
         </div>
